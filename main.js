cc7f9bb5564381bb5d5ef33b9a347dbcb6433da8
C0nfiducius <friedrich.albert@gmx.de>
2016-12-23 17:47:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
 
+  //Scroll Unternehmen
+  $('.scrollUnternehmen').click( function() {
+       $('html, body').animate({
+            scrollTop: $('#scrollToUnternehmen').offset().top
+       }, 400);
+  });
 
+  $('.scrollPortfolio').click( function() {
+       $('html, body').animate({
+            scrollTop: $('#scrollToPortfolio').offset().top
+       }, 400);
+  });
+
+  $('.scrollKontakt').click( function() {
+       $('html, body').animate({
+            scrollTop: $('#scrollToKontakt').offset().top
+       }, 400);
+  });
+
+
+
+
+
+
+
+
+
+
+
+  // Endscroll
+  if ($('.scrollToTop')[0]){
+       $('.scrollToTop').click(function(){
+            $('html,body').animate({ scrollTop: 0 }, 400);
+       });
+  }
 
 
 
